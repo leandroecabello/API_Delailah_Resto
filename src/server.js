9f100a2fbd31069ec1a325dcf8f115c40811965e
leandro <leandro.ecabello@gmail.com>
2020-04-21 08:08:04 -0300

    se modifica archivo server

--- src/server.js
+++ src/server.js
@@ -4,16 +4,20 @@ const Sequelize = require('sequelize')
 const dbQueryString = `mysql://${config.DDBB.USER}:${config.DDBB.PASS}@${config.DDBB.HOST}:${config.DDBB.PORT}/${config.DDBB.NAME}`
 const sequelize = new Sequelize(dbQueryString)
 
-sequelize
-    .authenticate()
-    .then(() => {
+const dbconnection = (async  () => {
+    try {
+        await sequelize.authenticate()
         console.log('Database connection has been established successfully.')
-        const app = require('./app/app')        
         
+        const app = require('./app/app')
+
         app.listen(config.SERVER_PORT, () => {
-            console.log(`Server is running at port ${config.SERVER_PORT}`)
+            console.log(`Server is running at port ${config.SERVER_PORT} `)
         })
-    })
-    .catch(err => console.error('Unable to connect to the database:', err))
+
+      } catch (err) {
+        console.error('Unable to connect to the database:', err)
+      }
+})()  
  
 module.exports = sequelize    
